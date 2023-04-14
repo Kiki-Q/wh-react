3d0aaf69dd009aa16dfe1b8147403440805ee35b
kiki.ding <dingqilove@iCloud.com>
2023-04-14 17:20:06 +0800

    feat(whole): init project react+ts+vite

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import * as path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  // 配置路径别名
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "https://yourBaseUrl",
+        changeOrigin: true,
+        cookieDomainRewrite: "",
+        secure: false,
+      },
+    },
+  },
+})
